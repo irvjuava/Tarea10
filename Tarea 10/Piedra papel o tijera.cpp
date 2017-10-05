ea484a83a2e31b07c2342017dfd35f7ad83fa12e
Irving Ricardo Juárez Vázquez <31520357+irvjuava@users.noreply.github.com>
2017-10-04 23:25:23 -0500

    Add files via upload

---
+++ Tarea 10/Piedra papel o tijera.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(){
+	
+	int p1,p2;
+	
+	printf("Bienvenido al mejor juego, revive tu infancia");
+	printf("\n1.Piedra");
+	printf("\n2.Papel");
+	printf("\n3.Tijera");
+	printf("\nElija sabiamente \n"); 
+	scanf("%d",&p1);
+	printf("\nElija sabiamente \n");
+	scanf("%d",&p2);
+	
+	if ((p1==1 && p2==1)||(p1==2 && p2==2)||(p1==3 && p2==3)){
+		
+		printf("Empate");
+	}
+	
+	if ((p1==1 && p2==2)||(p1==2 && p2==3)||(p1==3 && p2==1)){
+		
+		printf("El jugador 2 es lo maximo");
+	}
+	
+	if ((p1==1 && p2==3)||(p1==2 && p2==1)||(p1==3 && p2==2)){
+		
+		printf("El jugador 1 es lo maximo");
+	}
+	
+}
+ 
---
+++ Tarea 10/iva.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(int argc, char** argv) {
+	float total=0;
+	float iva=0;
+	float siniva=0;
+	
+		printf ("Inserte el precio\n");
+		scanf ("%f", &total);
+		iva=total*0.16;
+		siniva= total-iva;
+		printf ("El total es: %.2f \n ", total);
+		printf ("Precio sin iva: %.2f \n", siniva);
+		printf ("Total del iva: %.2f", iva); 
+		
+
+	
+}
